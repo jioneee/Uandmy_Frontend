26b61ebaa0ea607135253ce61f0b3b9e53864dd4
yoonji <kyjkbw@gmail.com>
2024-09-12 01:57:56 +0900

    [🎨style]: 스터디룸 관리 페이지 패딩값, bg 수정

--- src/components/common/CommonHeader.tsx
+++ src/components/common/CommonHeader.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 const CommonHeader = ({ title }: HeaderProps) => {
   return (
-    <header className="h-10 flex items-center mb-[.6875rem] text-lg text-[#212529] font-bold">
+    <header className="h-10 flex items-center pb-[.6875rem] text-lg text-[#212529] font-bold">
       {title}
     </header>
   );
